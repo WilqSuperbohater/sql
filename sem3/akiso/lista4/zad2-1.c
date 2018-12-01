fa7924348356ab7906661ddbba9e5dcaf65fa242
Tomek <42323517+WilqSuperbohater@users.noreply.github.com>
2018-12-01 19:22:20 +0000

    -niepotrzebne naglowki

--- sem3/akiso/lista4/zad2-1.c
+++ sem3/akiso/lista4/zad2-1.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
-#include <unistd.h>
 
-void skill_handler(int signum) {
-    printf("I am the God.");
-}
+void skill_handler(int signum) { printf("Bla bla bla..."); }
 
 int main() {
-    signal(9, skill_handler);
-    if(errno != 0)
-        printf("Ad 1: Nie mozna. Blad przy probie obslugi sygnalu 9.\n");
+   signal(9, skill_handler);
+   if(errno != 0)
+      printf("Ad 1: Nie mozna. Blad przy probie obslugi sygnalu 9.\n");
 }
